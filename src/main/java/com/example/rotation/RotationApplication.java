241a6269c0993df112eb5801bcd78c7a74be85b3
marzenusia <nsnjjs@com.pl>
2024-10-31 09:35:22 +0100

    Set database

---
+++ src/main/java/com/example/rotation/RotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.rotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rotation/RotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
